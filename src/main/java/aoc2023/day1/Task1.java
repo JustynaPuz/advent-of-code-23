630b31ab337ed7115e5d2c6ab6be64775114268b
Justyna Puz <puzjustyna@gmail.com>
2023-12-10 22:19:03 +0100

    Code refactor

--- src/main/java/aoc2023/day1/Task1.java
+++ src/main/java/aoc2023/day1/Task1.java
@@ -1,19 +1,14 @@
 package aoc2023.day1;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import aoc2023.Util.Util;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Main1 {
+public class Task1 {
 
     public static void main(String[] args) {
         String path = "inputDay1.txt";
-        List<String> lines = new ArrayList<>();
-
-        lines=Util.readFile(path);
+        List<String> lines = Util.readFile(path);
 
         int sum = 0;
 
--- src/main/java/aoc2023/day1/Task2.java
+++ src/main/java/aoc2023/day1/Task2.java
@@ -1,20 +1,19 @@
 package aoc2023.day1;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import aoc2023.Util.Util;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Main2 {
+public class Task2 {
 
-    static final String[] numbers = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+    static final String[] numbers = {"one", "two", "three", "four", "five", "six", "seven", "eight",
+            "nine"};
+
     static List<Integer> getNumberFromLine(String line) {
         List<Integer> numbersInLine = new ArrayList<>();
 
-        for (int i=0; i < line.length(); i++) {
-            for(int numIndex=0; numIndex < numbers.length; numIndex++) {
+        for (int i = 0; i < line.length(); i++) {
+            for (int numIndex = 0; numIndex < numbers.length; numIndex++) {
                 String num = numbers[numIndex];
                 if (line.startsWith(num, i)) {
                     numbersInLine.add(numIndex + 1);
@@ -28,9 +27,7 @@ public class Main2 {
 
     public static void main(String[] args) {
         String path = "inputDay1.txt";
-        List<String> lines = new ArrayList<>();
-
-        lines=Util.readFile(path);
+        List<String> lines = Util.readFile(path);
 
         int sum = 0;
 
